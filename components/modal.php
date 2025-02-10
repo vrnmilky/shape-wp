8140bc7b13bf9e8a880535ae926ac2a169e7cdb2
Pavel Borodin <vrn.milky@yandex.ru>
2025-02-10 19:41:25 +0300

    add modal / Contact Form 7

--- components/modal.php
+++ components/modal.php
@@ -2,14 +2,15 @@
         <div class="modal">
             <button class="close-btn close-modal">&times;</button>
             <h5 class="modal-title">Введите данные</h2>
-                <form class="modal-form">
-                    <input type="text" id="name" placeholder="Ваше имя" required>
+                <div class="modal-form">
+                    <!-- <input  type="text" id="name" placeholder="Ваше имя" required>
                     <input type="tel" id="phone" placeholder="Ваш номер телефона" required>
 
                     <div class="modal-buttons">
                         <button type="submit" class="submit-btn modal-button">Отправить</button>
                         <button type="button" class="cancel-btn modal-button close-modal">Отмена</button>
-                    </div>
-                </form>
+                    </div> -->
+                    <?php echo do_shortcode( '[contact-form-7 id="b1ef7ee" title="Контактная форма"] ') ?>
+                </div>
         </div>
     </div>
